8ad6b3fc890514264b83805c20183ab0c27479f6
Yassine <yassine@birig.me>
2025-11-30 23:45:05 +0100

    feat: add initial Firebase configuration and Android build setup.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.socialconnect.social_connect_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23  // Required for Firebase Auth
+        minSdk = flutter.minSdkVersion  // Required for Firebase Auth
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
