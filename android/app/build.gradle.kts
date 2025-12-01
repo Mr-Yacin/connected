ca04b05fda0dcddf2f09a4b628b91c79104adfbb
Yassine <yassine@birig.me>
2025-12-01 16:23:46 +0100

    feat: Bump project version and add Android build configuration including Firebase and Kotlin setup.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 android {
     namespace = "com.socialconnect.social_connect_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
